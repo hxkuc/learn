22ddd3055a3a04e69ea0ee460393713345f70ebe
huang <2374266244@qq.com>
2017-09-04 18:07:29 +0800

    首页获取列表

--- php/app/home/controller/Index.php
+++ php/app/home/controller/Index.php
@@ -5,7 +5,8 @@ use core\view\VIEW;
 /**
 * huang
 */
-class index extends base {
+class index extends base
+{
 	
 	/*public function index(){
 		$topiclist = DB::query("select * from topic");
@@ -13,7 +14,8 @@ class index extends base {
 		VIEW::display();
 	}*/
 
-	public function gettopiclist(){
+	public function gettopiclist()
+	{
 		$topic = new \home\model\topic;
 		$list = $topic->getlist();
 		$this->assign('success',1);
@@ -22,7 +24,8 @@ class index extends base {
 	}
 
 	// 获取首页用户信息
-	public function getindexuser(){
+	public function getindexuser()
+	{
 		$uid = $_REQUEST['uid'];
 		$user = new \user\model\user;
 		$userinfo = $user->getuserinfo($uid);
@@ -36,16 +39,26 @@ class index extends base {
 	}
 
 	// 获取广场用户列表
-	public function getuserlist(){
+	public function getuserlist()
+	{
 		$user = new \user\model\user;
 		$list = $user->userlist();
 		$this->assign('data', $list);
 		$this->assign('success',1);
 		$this->ajaxreturn();
 	}
+
+	// 获取最新的连接
+	public function getNewArticle()
+	{
+		$article = new \home\model\article;
+		$filter['status'] = 1;
+		$filter['currentpage'] = 1;
+		$filter['pagesize'] = 10;
+		$list = $article->getNewArticle($filter);
+		$this->assign('data', $list);
+		$this->assign('success',1);
+		$this->ajaxreturn();
+	}
 }
 
-
-
-
-?>
--- php/app/home/model/article.php
+++ php/app/home/model/article.php
@@ -4,11 +4,13 @@ use core\db\DB;
 /**
 * 文章model
 */
-class article{
+class article
+{
 	/*
 	*获取文章信息
 	*/
-	public function info($id){
+	public function info($id)
+	{
 		$info = DB::query('select * from `article` where id = '.$id);
 		return $info[0];
 	}
@@ -17,10 +19,17 @@ class article{
 	*列出文章列表
 	*$id主题id
 	**/
-	public function getlist($filter){
+	public function getlist($filter)
+	{
 		$pagenum = ($filter['currentpage']-1)*$filter["pagesize"];
-		$list = DB::query('select * from `article` where topicid = '.$filter["id"].' and status = '.$filter["status"].' limit '.$pagenum.','.$filter["pagesize"]);
-		$count = DB::query('select count(id) as num from `article` where topicid = '.$filter["id"].' and status = '.$filter["status"]);
+		$where = '1';
+		$where .= isset($filter["id"]) && $filter["id"] ? ' and topicid = '.$filter["id"] : '';
+		$where .= isset($filter["status"]) && $filter["status"] ? ' and status = '.$filter["status"] : '';
+
+		$order = ' order by addtime desc';
+
+		$list = DB::query('select * from `article` where '.$where.$order.' limit '.$pagenum.','.$filter["pagesize"]);
+		$count = DB::query('select count(id) as num from `article` where '.$where);
 		return array(
 			"list"=>$list,
 			"count"=>$count['0']['num'],
@@ -29,7 +38,8 @@ class article{
 	/*
 	*列出文章列表（列出文章所属主题和推送人--后台使用）
 	*/
-	public function getarticlelist($filter){
+	public function getarticlelist($filter)
+	{
 		$where = 'a.status = '.$filter["status"].' and a.uid = '.$filter['uid'];
 		if($filter["topicid"]){
 			$where .= ' and a.topicid = '.$filter["topicid"];
@@ -47,21 +57,41 @@ class article{
 	}
 
 	//新增文章
-	public function add($data){
+	public function add($data)
+	{
 		$res = DB::exec('insert into `article` (topicid,articlename,articleurl,addtime,status,uid,adduid)
 		 values ('.$data["topicid"].',"'.$data["articlename"].'","'.$data["articleurl"].'",'.time().','.$data['status'].','.$data['uid'].','.$data['adduid'].')');
 		return $res;
 	}
 	//删除文章
-	public function delete($id){
+	public function delete($id)
+	{
 		$res = DB::exec('delete from `article` where id = '.$id);
 		return $res;
 	}
 
 	// 改变状态
-	public function changestatus($data){
+	public function changestatus($data)
+	{
 		$res = DB::exec('update `article` set status = '.$data['status'].' where id = '.$data['id']);
 		return $res;
+	}
+
+	// 列出首页的最新文章
+	public function getNewArticle($filter)
+	{
+		$pagenum = ($filter['currentpage']-1)*$filter["pagesize"];
+		$where = '1';
+		$where .= isset($filter["id"]) && $filter["id"] ? ' and topicid = '.$filter["id"] : '';
+		$where .= isset($filter["status"]) && $filter["status"] ? ' and status = '.$filter["status"] : '';
+
+		$order = ' order by a.addtime desc';
+
+		$list = DB::query('
+			select a.articlename,a.articleurl,a.addtime,u.username,u.headimg from `article` a left join `user` u on a.uid = u.id where '.$where.$order.' limit '.$pagenum.','.$filter["pagesize"]
+			);
+
+		return $list;
 	}
 }
 
